8ef0ed2bf3cde83f9f068a90f5083608c20bbd21
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-10-14 16:22:44 +0530

    updated gaurd

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,22 +2,27 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, Routes } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth:AuthService,private router:Router){}
+  session;
+  constructor(private auth:AuthService,private router:Router,private http:HttpClient){this.http.get("https://social--auth.herokuapp.com/details").subscribe((d)=>
+  {
+    this.session=d;
+  })}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  
-      if(this.auth.user){
+      if(this.session){
         return true;
       }
     else {
       this.router.navigateByUrl('/login');
       return false;
     }
+
 } 
+
 }
