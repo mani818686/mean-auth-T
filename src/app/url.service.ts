50eaf4e534ae503f09646dd6f09d5482696a87e1
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-10-18 14:39:28 +0530

    updated

--- src/app/url.service.ts
+++ src/app/url.service.ts
@@ -18,8 +18,12 @@ export class UrlService {
    async getUrl()
   { 
     let status=await this.http.get('https://social--auth.herokuapp.com/api/session').toPromise();
-    let user=status["user"].name;
-    let d=await this.http.get("https://social--auth.herokuapp.com/urls/"+user).toPromise();
+    let user=status["user"];
+    console.log(user);
+    console.log(user["name"]);
+    
+    
+    let d=await this.http.get("https://social--auth.herokuapp.com/urls/"+user["name"]).toPromise();
     console.log(d["data"]);
     this.urls.next(d["data"]);
   }
